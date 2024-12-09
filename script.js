ae4060f4c03b9606282e81723b5e9845b39c85f3
adam1098765432 <157524663+adam1098765432@users.noreply.github.com>
2024-12-08 23:32:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const darkModeToggle = document.getElementById('dark-mode-toggle');
 
 // Check the user's theme preference in localStorage
 if (localStorage.getItem('theme') === 'light') {
-    document.body.classList.add('light-mode'); // Start in light mode if saved as light
+    document.body.classList.add('light-mode');
 } else {
-    document.body.classList.add('dark-mode'); // Default to dark mode
+    document.body.classList.add('dark-mode');
 }
 
-// Toggle dark mode and light mode on button click
+// Toggle between dark and light mode
 darkModeToggle.addEventListener('click', () => {
     if (document.body.classList.contains('dark-mode')) {
         document.body.classList.remove('dark-mode');
         document.body.classList.add('light-mode');
-        localStorage.setItem('theme', 'light'); // Save the preference
+        localStorage.setItem('theme', 'light');
     } else {
         document.body.classList.remove('light-mode');
         document.body.classList.add('dark-mode');
-        localStorage.setItem('theme', 'dark'); // Save the preference
+        localStorage.setItem('theme', 'dark');
     }
 });
